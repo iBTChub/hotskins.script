782bf6b8198195e7c888579930681615c0e4782e
ua.lifesheets <ua.lifesheets@icloud.com>
2024-09-07 19:05:58 +0300

    Automatic loading of classes and function!

--- platform/requires/connect.php
+++ platform/requires/connect.php
@@ -1,3 +1,30 @@
 <?php
 
-// Source code
+# Load all PHP files from the functions directory
+try {
+    foreach (glob(PLATFORM . '/functions/*.php') as $file) {
+        if (file_exists($file)) {
+            require_once $file;
+        }
+    }
+} catch (Exception $e) {
+    # Handle exceptions related to file inclusion
+    error_log('Error including function files: ' . $e->getMessage());
+    echo 'An error occurred while loading function files.';
+}
+
+# Autoload PHP classes
+spl_autoload_register(function ($class_name) {
+    try {
+        $file = PLATFORM . '/classes/' . $class_name . '.class.php';
+        if (file_exists($file)) {
+            require_once $file;
+        } else {
+            throw new Exception("Class file $file does not exist.");
+        }
+    } catch (Exception $e) {
+        # Handle exceptions related to class autoloading
+        error_log('Error loading class: ' . $e->getMessage());
+        echo 'An error occurred while loading class files.';
+    }
+});
