be04a62820e191c751dab0685c21cd5ba15993d1
ua.lifesheets <ua.lifesheets@icloud.com>
2024-09-07 19:07:03 +0300

    Root directory and platform definitions.

--- platform/requires/nucleus.php
+++ platform/requires/nucleus.php
@@ -1,3 +1,15 @@
 <?php
 
-// Source code
+/**
+ * Define the root path of the document root directory.
+ * This constant represents the base directory for all relative paths.
+ */
+
+define('ROOT', $_SERVER['DOCUMENT_ROOT']);
+
+/**
+ * Define the platform path relative to the root directory.
+ * This constant specifies the path to the platform directory where application files are located.
+ */
+
+define('PLATFORM', ROOT . '/platform');
